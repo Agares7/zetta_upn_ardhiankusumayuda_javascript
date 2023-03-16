98065b534e14fc99d2565082c878574232f99cbd
Agares7 <ardhiankusumayuda@gmail.com>
2023-03-16 13:55:48 +0700

    js day2

--- main.js
+++ main.js
@@ -4,19 +4,39 @@ const book = (
     author:"Ryōgo Narita"}
 );
 
-function bookpurchase (name, indc, intax) {
+function bookpurchase (name, indc, intax, stock, purchased) {
 
     const price = book.price
     const dc = price * (indc/100);
     const dcprice = price - dc;
     const tax = dcprice * (intax/100);
-    const total = dcprice + tax;
+    const taxprice = dcprice + tax;
+    let totalPrice = 0;
+    let stockLeft = stock;
+
+    for(let i = 0; i < purchased; i++){
+        if(stockLeft == 0){
+            console.log("Sorry, book is out of stock");
+            break;
+        }
+        totalPrice += taxprice;
+        stockLeft--;
+    }
+
 
     console.log('Original Price', price);
     console.log('Discount Amount', dc);
     console.log('Price after Discount', dcprice);
     console.log('Tax Amount', tax);
-    console.log('Price after Tax', total);
+    console.log('Price after Tax', taxprice);
+    console.log('Purchased', purchased);
+    console.log('Total Price', totalPrice);
+
+    if (stockLeft > 0) {
+        console.log(`There are ${stockLeft} book left in stock`);
+      } else {
+        console.log(`Sorry, book is out of stock.`);
+      }
 }
 
-bookpurchase(book, 10, 10);
+bookpurchase(book, 10, 10, 10, 5);
